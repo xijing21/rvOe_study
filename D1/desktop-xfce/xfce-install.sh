8f9ed3a8185475b4b0bb2e271c01387c3ee3d285
xijing <84754181+xijing21@users.noreply.github.com>
2021-10-27 22:16:05 +0800

    xfce desktop test

--- D1/desktop-xfce/xfce-install.sh
+++ D1/desktop-xfce/xfce-install.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 set -v
 
-df -h  2>&1|tee -a xfce-yum-install.log
-fdisk -l 2>&1|tee -a xfce-yum-install.log
-date 2>&1|tee -a xfce-yum-install.log
-cat /etc/yum.repos.d/oe-rv.repo 2>&1|tee -a xfce-yum-install.log
+df -h  
+fdisk -l 
+date 
+cat /etc/yum.repos.d/oe-rv.repo 
 
 echo 'install Xfce Desktop 15 packages'
-yum -y install libxfce4util  2>&1|tee -a xfce-yum-install.log
-yum -y install xfconf  2>&1|tee -a xfce-yum-install.log
-yum -y install libxfce4ui 2>&1|tee -a xfce-yum-install.log
-yum -y install exo  2>&1|tee -a xfce-yum-install.log
-yum -y install garcon  2>&1|tee -a xfce-yum-install.log
-yum -y install thunar  2>&1|tee -a xfce-yum-install.log
-yum -y install thunar-volman  2>&1|tee -a xfce-yum-install.log
-yum -y install tumbler  2>&1|tee -a xfce-yum-install.log
-yum -y install xfce4-appfinder    2>&1|tee -a xfce-yum-install.log
-yum -y install xfce4-panel  2>&1|tee -a xfce-yum-install.log
-yum -y install xfce4-power-manager 2>&1|tee -a xfce-yum-install.log
-yum -y install xfce4-settings 2>&1|tee -a xfce-yum-install.log
-yum -y install xfdesktop 2>&1|tee -a xfce-yum-install.log
-yum -y install xfwm4 2>&1|tee -a xfce-yum-install.log
-yum -y install xfce4-session 2>&1|tee -a xfce-yum-install.log
+yum -y install libxfce4util  
+yum -y install xfconf  
+yum -y install libxfce4ui 
+yum -y install exo  
+yum -y install garcon  
+yum -y install thunar  
+yum -y install thunar-volman  
+yum -y install tumbler  
+yum -y install xfce4-appfinder    
+yum -y install xfce4-panel  
+yum -y install xfce4-power-manager 
+yum -y install xfce4-settings 
+yum -y install xfdesktop 
+yum -y install xfwm4 
+yum -y install xfce4-session 
 
 
 echo 'install Xfce Applications 6 packages'
-yum -y install parole 2>&1|tee -a xfce-yum-install.log
-yum -y install xfce4-terminal 2>&1|tee -a xfce-yum-install.log
-yum -y install xfburn 2>&1|tee -a xfce-yum-install.log
-yum -y install ristretto 2>&1|tee -a xfce-yum-install.log
-yum -y install xfce4-notifyd 2>&1|tee -a xfce-yum-install.log
-yum -y install xfce4-pulseaudio-plugin 2>&1|tee -a xfce-yum-install.log
+yum -y install parole 
+yum -y install xfce4-terminal 
+yum -y install xfburn 
+yum -y install ristretto 
+yum -y install xfce4-notifyd 
+yum -y install xfce4-pulseaudio-plugin 
